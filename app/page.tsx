c3266440649b307b3ebd72e716edde58c8047567
Konstantin Razdolski <haridula@proton.me>
2026-02-18 00:51:38 +0200

    Mobile support

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import MotivationLetter from "@/markdown/MotivationLetter.mdx";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <div className="w-1/3 mx-auto py-10">
+    <div className="min-h-screen flex flex-col">
+      <div className="w-1/3 max-sm:hidden max-md:hidden mx-auto py-10 flex-grow">
         <MotivationLetter />
       </div>
-      <footer className="text-center text-subtext absolute bottom-4 w-full">
+      <div className="md:hidden w-4/5 mx-auto py-10 flex-grow">
+        <MotivationLetter />
+      </div>
+      <footer className="text-center text-subtext w-full py-6">
         2026 Konstantin Razdolski
       </footer>
     </div>
